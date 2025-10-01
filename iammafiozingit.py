1b9b8e49e5f368130f98d2d21fad34aca3b78e45
user <sanababaduk83@gmail.com>
2025-10-01 21:04:03 +0300

    lesson 5 i guess

--- iammafiozingit.py
+++ iammafiozingit.py
@@ -1,10 +1,21 @@
-data = list(map(int, input().split()))
+#дз номер 6
+result = []
 
-n = data[0]
-weights = data[1:]
+def divider(a, b):
+    if a < b:
+        raise ValueError
+    if b > 100:
+        raise IndexError
+    return a / b
 
-max_w = max(weights)
-min_w = min(weights)
-total = sum(weights)
+data = {10: 2, 2: 5, "1234234772834": 4, 18: 0, (): 15, 8: 4}
 
-print(max_w, min_w, total)
+for key in data:
+    try:
+        res = divider(key, data[key])
+        result.append(res)
+    except Exception as e:
+        print(f"Виняток для пари ({key}:{data[key]}): {type(e).__name__} - {e}")
+
+print(result)
+           #kem kem? може key? a так все гуд
