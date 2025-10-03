7274c7a17600dd4952d02a911c9fb99454824f5f
user <sanababaduk83@gmail.com>
2025-10-04 01:50:49 +0300

    lesson 5 i guess

--- iammafiozingit.py
+++ iammafiozingit.py
@@ -1,21 +1,11 @@
-#дз номер 6
-result = []
+import random
+import string
 
-def divider(a, b):
-    if a < b:
-        raise ValueError
-    if b > 100:
-        raise IndexError
-    return a / b
+def random_letters():
+    while True:
+        yield random.choice(string.ascii_lowercase)
 
-data = {10: 2, 2: 5, "1234234772834": 4, 18: 0, (): 15, 8: 4}
-
-for key in data:
-    try:
-        res = divider(key, data[key])
-        result.append(res)
-    except Exception as e:
-        print(f"Виняток для пари ({key}:{data[key]}): {type(e).__name__} - {e}")
-
-print(result)
-           #kem kem? може key? a так все гуд
+gen = random_letters()
+for _ in range(87877842348):
+    print(next(gen), end=" ")
+  #дужеее багато букв...
